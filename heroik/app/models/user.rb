f8f7df8062e5bca00211b029b9642b33a3ea8d06
cmtaubman <carmen@cgkcorporation.com>
2016-03-10 15:49:40 +0800

    models updated

--- heroik/app/models/user.rb
+++ heroik/app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
           :recoverable, :rememberable, :trackable, :validatable,
           :omniauthable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :posts
+  has_many :comments
+  has_many :votes
 end
