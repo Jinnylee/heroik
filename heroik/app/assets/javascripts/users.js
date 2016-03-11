ba7655eb92fd2c8d2a44bbbea75a2c96799e0761
cmtaubman <carmen@cgkcorporation.com>
2016-03-11 10:49:57 +0800

    j toker installed

--- heroik/app/assets/javascripts/users.js
+++ heroik/app/assets/javascripts/users.js
@@ -1,32 +1,34 @@
 
 // This is just testing to make sure the code works. The below code should be added to the NAV BAR in layouts
+$(document).ready(function () {
 
-$('#signup-form').on('submit', function(e){
-    e.preventDefault();
-
-  $.auth.emailSignUp({
-    email: $('#signup-form input[name="email"]').val(),
-    password: $('#signup-form input[name="password"]').val(),
-    password_confirmation: $('#signup-form input[name="password_confirmation"]').val()
-  }).then(function(user){
-    console.log(user)
-    window.location.href = "www.google.com";
-  }).fail(function(resp){
-    console.log(resp)
+  $('#signup-form').on('submit', function(e){
+      e.preventDefault();
+console.log("clicked");
+    $.auth.emailSignUp({
+      email: $('#signup-form input[name="email"]').val(),
+      password: $('#signup-form input[name="password"]').val(),
+      password_confirmation: $('#signup-form input[name="password_confirmation"]').val()
+    }).then(function(user){
+      console.log(user)
+      window.location.href = "www.google.com";
+    }).fail(function(resp){
+      console.log(resp)
+    });
   });
-});
 
-  $('#signin-form').on('submit', function(e){
-    e.preventDefault();
+    $('#signin-form').on('submit', function(e){
+      e.preventDefault();
 
-  $.auth.emailSignIn({
-    email: $('#signin-form input[name="email"]').val(),
-    password: $('#signin-form input[name="password"]').val(),
-  }).then(function(user){
-    console.log(user)
-    window.location.href = "www.google.com";
-    console.log(user)
-  }).fail(function(resp){
-    console.log(resp)
+    $.auth.emailSignIn({
+      email: $('#signin-form input[name="email"]').val(),
+      password: $('#signin-form input[name="password"]').val(),
+    }).then(function(user){
+      console.log(user)
+      window.location.href = "www.google.com";
+      console.log(user)
+    }).fail(function(resp){
+      console.log(resp)
+    });
   });
 });
