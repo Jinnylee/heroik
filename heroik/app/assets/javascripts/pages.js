c1ce6aa30e32e6a0bd781256f75aab43c4a96555
cmtaubman <carmen@cgkcorporation.com>
2016-03-15 12:50:08 +0800

    carousel changes

--- heroik/app/assets/javascripts/pages.js
+++ heroik/app/assets/javascripts/pages.js
@@ -8,4 +8,28 @@ $(document).ready(function () {
       window.location.href = '/posts?target=environment'
   });
 
+  $('#carousel-button-2').off().on('click', function(e){
+      e.preventDefault();
+      console.log("clicked env button");
+      window.location.href = '/posts?target=community'
+  });
+
+  $('#carousel-button-3').off().on('click', function(e){
+      e.preventDefault();
+      console.log("clicked env button");
+      window.location.href = '/posts?target=youth'
+  });
+
+  $('#carousel-button-4').off().on('click', function(e){
+    e.preventDefault();
+    console.log("clicked env button");
+    window.location.href = '/posts?target=neighbourhood'
+  });
+
+  $('#carousel-button-5').off().on('click', function(e){
+    e.preventDefault();
+    console.log("clicked env button");
+    window.location.href = '/posts?target=mostpopular'
+  });
+
 });
