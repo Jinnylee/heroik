5ca5d87a948a36665bc0357a3437b66caad04ccf
Jinny Lee <Jinnylee@Jinnys-MacBook-Air.local>
2016-03-15 15:05:50 +0800

    finished look of comments section

--- heroik/app/assets/javascripts/comments.js
+++ heroik/app/assets/javascripts/comments.js
@@ -63,22 +63,22 @@
       error: function(response) {
         console.log(response);
       }
-    })
-  }
+    });
+  };
 
 // APPEND COMMENTS ON POST MODEL (limit to 5)
   var appendComments = function(image, username, comment, id, user_id) {
     var commentHTML =
-    '<div class="comment">' +
-      '<img src="' + image + '" class="comment-user-image" >' +
-      '<div class="comment-user">' + username + '</div>' +
-      '<div class="comment-text">' + comment + '</div>' +
+    '<div class="comment row">' +
+      '<div class="comment-user-image col-xs-2"><img src="' + image + '" onerror="this.src=\'http://camaleon.tuzitio.com/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png\'" class="comment-image-raw"></div>' +
+      '<div class="col-xs-8 comment-user-div"><div class="comment-user col-xs-12">' + username + '</div>' +
+      '<div class="comment-text col-xs-12">' + comment + '</div></div>' +
     '</div>'
 
     var $comment = $(commentHTML)
 
     if (window.userID == user_id) {
-      $comment.append('<button type="button" class="btn btn-default deleteCommentBtn" data-id="' + id + '"><i class="fa fa-trash-o"></i></button>')
+      $comment.append('<div class="trash-btn col-xs-2"><button type="button" class="btn btn-default deleteCommentBtn" data-id="' + id + '"><i class="fa fa-trash-o"></i></button></div>')
     }
 
     $('#allcomments').append($comment);
