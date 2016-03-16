8dfd5807838a63177fe45ab3db3c9fd232c77a27
Shawn Alwani <shawn.alwani@gmail.com>
2016-03-16 11:16:45 +0800

    updated image sizing

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ActiveRecord::Base
   has_many :votes
   has_many :comments
 
-  has_attached_file :image
+  has_attached_file :image, styles: {
+    medium: "300x300>",
+  }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,9 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :votes
 
-  has_attached_file :avatar
+  has_attached_file :avatar, styles: {
+    medium: "300x300>",
+  }
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
