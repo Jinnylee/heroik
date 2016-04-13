f935eb97d75805b8b8d908d8db75eba270ddbc89
Jinny Lee <Jinnylee@Jinnys-MacBook-Air.local>
2016-04-13 13:20:02 +0800

    url change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,7 @@
 $.auth.configure({
   // By default, you only need to configure apiUrl
   // Note that if you put a '/' at the end of the link, there will be errors when calling the api
-  apiUrl: 'http://localhost:3000',
+  apiUrl: location.origin,
   forceHardRedirect: true,
   authProviderPaths: {
     facebook:  '/auth/facebook',
